19c20cf5c0cb23312003bdaea048c382a52c1a90
EC2 Default User <ec2-user@ip-172-31-19-2.us-east-2.compute.internal>
2018-07-12 01:07:08 +0000

    welcome page responsive edit

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -1,8 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+   <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+       <title>Lunch Meter</title>
+
+<!-- Bootstrap CSS-->
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+       <link rel="stylesheet" href="{{ secure_asset('/css/posts.css') }}">
+       <!-- jQuery -->
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+       <!-- Bootstrap JavaScript-->
+       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
 <ul class="media-list">
 @foreach ($posts as $post)
     <?php $user = $post->user; ?>
     <div class="panel panel-primary">
-        <div class="panel-body">
+        
             <li class="media">
                 <div class="media-body">
                     <div>
@@ -25,9 +43,10 @@
                     @endif
                 </div>
             </li>
-        </div>
+     
 
     </div>
+    <br>
 @endforeach
 </ul>
 {!! $posts->render() !!}
