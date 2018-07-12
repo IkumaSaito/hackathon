103e0f6a7791c1b7a615e80bcb0e07609956f88b
EC2 Default User <ec2-user@ip-172-31-19-2.us-east-2.compute.internal>
2018-07-12 05:39:36 +0000

    timeline edit2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,15 +14,10 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    
     </head>
     <body>
         @include('commons.navbar')
-
-
-            @include('commons.error_messages')
-
-            @yield('content')
-       
+        @include('commons.error_messages')
+        @yield('content')
     </body>
 </html>
--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -1,22 +1,6 @@
-@extends('layouts.app')
 
-<!DOCTYPE html>
-<html lang="ja">
-   <head>
-       <meta charset="utf-8">
-       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-       <meta name="viewport" content="width=device-width, initial-scale=1">
-       
-       <title>Lunch Meter</title>
-
-<!-- Bootstrap CSS-->
-       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
        <link rel="stylesheet" href="{{ secure_asset('/css/posts.css') }}">
-       <!-- jQuery -->
-       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-       <!-- Bootstrap JavaScript-->
-       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+     
 
 <ul class="media-list">
 @foreach ($posts as $post)
