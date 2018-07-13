19424a65773ee774feac58ff79a863e56c0bc36a
EC2 Default User <ec2-user@ip-172-31-19-2.us-east-2.compute.internal>
2018-07-13 07:50:59 +0000

    dm page edit

--- resources/views/directmessages/allusers.blade.php
+++ resources/views/directmessages/allusers.blade.php
@@ -1,18 +1,20 @@
+ <link rel="stylesheet" href="{{ secure_asset('/css/allusers.css') }}">
+ 
 @if (count($senders) > 0)
+<div class="aside">
 <ul>
 @foreach ($senders as $key => $sender)
     <li>
         <div class="media-body">
             <div>
-                {!! link_to_route('users.directmessages', $sender->name, ['id' => $sender->id]) !!}
-                <?php echo count($unseens->where('user_id', $sender->id)); ?>        
+                {!! link_to_route('users.directmessages', $sender->name, ['id' => $sender->id]) !!}  :
+                <?php echo count($unseens->where('user_id', $sender->id)); ?>件    
             </div>
         </div>
     </li>
 @endforeach
 </ul>
-
-
+</div>
 
 @endif
 
