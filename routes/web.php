faed718b31627f379ef49a8d1f75d870b9286450
EC2 Default User <ec2-user@ip-172-31-22-167.us-east-2.compute.internal>
2018-07-18 05:47:23 +0000

    explain

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,5 @@ Route::group(['middleware' => 'auth'], function () {
 
 Route::get('intro', 'UsersController@intro')->name('users.intro');
 Route::get('introja', 'UsersController@introja')->name('users.introja');
-    
+
+Route::get('explain', 'UsersController@explain')->name('users.explain');    
