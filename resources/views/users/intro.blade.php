cfc195be5d3ca719fb6e44e96879bd9fbb8f8851
EC2 Default User <ec2-user@ip-172-31-25-135.us-east-2.compute.internal>
2018-07-19 03:12:56 +0000

    my21 done

--- resources/views/users/intro.blade.php
+++ resources/views/users/intro.blade.php
@@ -1,8 +1,8 @@
 
-
 <!DOCTYPE html>
-<html lang="utf-8">
-    <head>
+<html lang="ja">
+
+<head>
         <meta charset="utf-8">
         <title>Introduction</title>
         <!--Lunch menter-->
@@ -12,34 +12,29 @@
         <!--meet *people-->
         <link href="https://fonts.googleapis.com/css?family=Cormorant+SC|Galada|Harmattan|Pacifico|Petit+Formal+Script|Playfair+Display|Roboto|Roboto+Mono" rel="stylesheet">
         　　　<!--コンセプト-->
-        　　　　<link href="https://fonts.googleapis.com/css?family=Cormorant+SC|Galada|Harmattan|Pacifico|Petit+Formal+Script|Playfair+Display|Roboto|Roboto+Mono|Shrikhand" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Cormorant+SC|Galada|Harmattan|Pacifico|Petit+Formal+Script|Playfair+Display|Roboto|Roboto+Mono|Shrikhand" rel="stylesheet">
         <!--p-->
         <link href="https://fonts.googleapis.com/css?family=Cormorant+SC|Galada|Harmattan|Pacifico|Petit+Formal+Script|Playfair+Display|Roboto|Roboto+Mono|Shrikhand" rel="stylesheet">
         <link rel="stylesheet" href="css/hakason.css">
-   
         <!--jpfont-->
         <link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet" />
         <!--日本語-->
         <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet" />
         <!--簡単登録無料のところ-->
         <link href="https://fonts.googleapis.com/css?family=Gaegu" rel="stylesheet">
-        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="css/hakason15.css">
-    </head>
+</head>
+
     
-      
-      
-    <body>
-        <div class="container-fluied">
-            <div class='row'>
-                <div class="top">
+<body>      
+                    <img src="images/top.jpg" class="img1"> 
                     <div class ="a">
                         <div class='concept'>
-                        
-                        <div class="senpai">What is LUNCH MEETER?</div>
+                            <div class="senpai">What is LUNCH MEETER?
+                            </div>
                             <div class='message'>
                                 <p>Don’t you feel a little lonely having lunch alone and want to ask someone to have lunch with you?</p>
                                 <p>Would you like to learn a foreign language from a native or experience cross-cultural exchange?</p>
@@ -48,18 +43,14 @@
                             </div>
                         </div>
                     </div>
-
-                 </div>
-                    <img class="img-responsive" src="images/top.jpg"> 
-            </div>
-
+                    
             <h1 class="container_h1">LunchMeeter's point</h1>
             <br>
             <br>
             <br>
             
             
-            
+        <footer>
             <div class="row">
                <div class="col-md-4 col-sm-offset-2">
                        <img src="images/hakason2.jpg" class="img-circle_lm" 
@@ -103,10 +94,6 @@
             
             
             
-
-        
-        <footer>
-            
             </div>
             
  
@@ -117,13 +104,13 @@
                 <br>
                Make a profile.
                 </div>
-                <img src="images/yazirushi.jpg" width="100" height="60">
+                <!--<img src="images/yazirushi.jpg" width="100" height="60">-->
         
         <div class="col-md-3">
             <img src="images/&3.jpg" width="240px" height="500px"><br>
                   Find a person.
              </div>
-             <img src="images/yazirushi.jpg" width="100" height="60">
+             <!--<img src="images/yazirushi.jpg" width="100" height="60">-->
 
         <div class="col-md-3">
              <img src="images/&4.jpg"width="240px" height="500px"><br>
@@ -141,6 +128,12 @@
                 
             
         </footer>
-    </body>
+</body>
+
+</html>   
     
-</html>
+    
+    
+    
+    
+            
