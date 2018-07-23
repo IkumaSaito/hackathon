da74ca757ee576f37daf80ecc507af9bdc9ac579
EC2 Default User <ec2-user@ip-172-31-19-2.us-east-2.compute.internal>
2018-07-23 03:57:41 +0000

    cal2

--- resources/views/calendar/calendar.blade.php
+++ resources/views/calendar/calendar.blade.php
@@ -109,7 +109,7 @@ function getNthDay($year, $month, $day, $n) {
         <th><?php echo $year;?> / <?php echo $month;?></td>
         <!--<th colspan="2"><a href="<?php $_SERVER['SCRIPT_NAME'];?>?date=<?php echo $next_week;?>">next week &raquo;</a></td>-->
     </tr>
-    <tr>
+    <tr class="day_of_week">
         <td>Sun</td>
         <td>Mon</td>
         <td>Tue</td>
@@ -153,7 +153,7 @@ function getNthDay($year, $month, $day, $n) {
 
 <style type="text/css">
 table {
-    max-width: 350px;
+    width: 300px;
     margin-left: auto;
     margin-right: auto;
     border-style: none;
@@ -170,10 +170,19 @@ table td {
     padding: 5px;
 }
 
-tbody{
-    max-width:100%;
+@media screen and (min-width: 800px) { 
+    table{
+        width:700px;
+    }
+    
 }
 
+tr.day_of_week td:first-child{
+           background:#ffb6c1;
+       }
+tr.day_of_week td:last-child{
+           background:#add8e6;
+       } 
 </style>
 
 
